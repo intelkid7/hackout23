7edcf00f509f28df6b380a4b74af623a409a4fa5
intelkid7 <priyanshjsyadav777@gmail.com>
2023-08-26 13:18:40 +0530

    logincomp

--- hims/src/components/Form.jsx
+++ hims/src/components/Form.jsx
@@ -3,47 +3,32 @@ import React from 'react'
 export default function Form() {
   return (
     <div>
-      <form>
-        <fieldset >
-          <legend>DOCTOR LOGIN</legend>
-          <div className="mb-3">
-            <label htmlFor="disabledTextInput" className="form-label">
-              E-mail 
-            </label>
-            <input
-              type="text"
-              id="disabledTextInput"
-              className="form-control"
-              placeholder="Email"
-            />
+      <div id='logdiv'>
+        <div id='logform'>
+          <div id='logimgdiv'>
+            <img src="logolight.png" id='logimg' />
+            <h1 id='moto'>Empowering Healthcare</h1>
           </div>
-          <div className="mb-3">
-            <label htmlFor="disabledSelect" className="form-label">
-              Password
-            </label>
-            <input type="password" id='pass' />
-          </div>
-          <div className="mb-3">
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                id="disabledFieldsetCheck"
-                disabled
-              />
-              <label
-                className="form-check-label"
-                htmlFor="disabledFieldsetCheck"
-              >
-                Can't check this
-              </label>
+          <form action="" id='logform2'>
+            <h1 id='reghead'>Login</h1>
+            <div id='seperator'></div>
+            <input type="Email" id="loginput" placeholder="example@gmail.com" />
+            <div id='seperator'></div>
+            <input type="Password" id="loginput" placeholder="enter your password" />
+            <div id='seperator'></div>
+            <div id='seperator'></div>
+            <h1 id="reghead">Choose your role</h1>
+            <div id="rolediv">
+              <input name="Role" id="role" type="radio"></input>
+              <label id="labela">Admin</label>
+              <div id='seperator2'></div>
+              <input name="Role" id="role" type="radio"></input>
+              <label id="labela">Doctor</label>
             </div>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </fieldset>
-      </form>
+            <button id="subbut">Submit</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
