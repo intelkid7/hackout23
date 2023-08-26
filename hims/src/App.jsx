71b2498db64853f1109e6b3d10ca809b60a7bb9c
Krish Patel <krishpatel085@gmail.com>
2023-08-26 18:04:42 +0530

    Patient_dia

--- hims/src/App.jsx
+++ hims/src/App.jsx
@@ -4,11 +4,11 @@ import './App.css'
 import Form from './components/form'
 import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
-import Registration from './components/registration';
 import Doctor_home from './components/Doctor_home';
 import Registration from './components/registration'; 
 import toast, { Toaster } from 'react-hot-toast';
 import FindPatient from './components/FindPatient';
+import Patient_dia from './components/patient_dia';
 
 
 function App() {
@@ -20,7 +20,7 @@ function App() {
       <Route path="/Login" element={<Form />} />
       <Route path="/Registration" element={<Registration />} />
       <Route path="/Doctor_home" element={<Doctor_home />} />
-      <Route path="/patient_dia" element={<patient_dia />} />
+      <Route path="/Patient" element={<Patient_dia />} />
       <Route path="/BookPatient" element={<FindPatient />} />
       <Route path='*' element={<h1>404 Not Found</h1>} />
     </Routes>
