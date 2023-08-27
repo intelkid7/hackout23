6b31c5166152e144ad0ba65961548752262eaf00
intelkid7 <priyanshjsyadav777@gmail.com>
2023-08-27 12:12:05 +0530

    update05

--- hims/src/components/Doctor_home.jsx
+++ hims/src/components/Doctor_home.jsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import Navbar from './Navbar';
 
 export default function Doctor_home() {
@@ -10,14 +10,14 @@ export default function Doctor_home() {
 
   const getPatients = async () => {
     const res = await axios.get(`${import.meta.env.VITE_REACT_API_APP_PORT}/api/v1/auth/getRegisteredPatient`);
-    console.log(res); 
+    console.log(res);
     setPatient(res.data.data);
 
   }
 
   const handleDelete = async (id) => {
     const res = await axios.delete(`${import.meta.env.VITE_REACT_API_APP_PORT}/api/v1/auth/deletePatient/${id}`)
-    console.log(res); 
+    console.log(res);
     setPatient(patient.filter((p) => p._id !== id));
   }
 
@@ -26,11 +26,18 @@ export default function Doctor_home() {
   }, []);
 
   return (
-    <div style={{padding: "3rem"}}>
+    <div id='homecont'>
       <Navbar></Navbar>
       <div id='tdiv'>
-        <table style={{color:"white"}}>
-          <tr>Hello</tr>
+        <table id='ptable'>
+          <tr id='trow'>
+            <th id='thead'>Patient Number</th>
+            <th id='thead'>Patient Name</th>
+          </tr>
+          <tr id='trow'>
+            <td>1</td>
+            <td>John</td>
+          </tr>
         </table>
       </div>
     </div>
